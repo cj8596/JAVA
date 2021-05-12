8198644c176414c844265ddb5519d77cc043232c
Cristin Johnson <cristinjohnson96@gmail.com>
2021-05-12 18:59:26 +0530

    Update and rename first.java to firsttask.java

--- firsttask.java
+++ firsttask.java
@@ -1,6 +1,6 @@
 package demo;
-
-public class first {
+// MyCaptain May 2021 Java Workshop Task1: Hello World
+public class firsttask {
 
 	public static void main(String[] argsA) {
 		System.out.println("Hello World!");
